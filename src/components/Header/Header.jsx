e7140b19b4d3478d6dfba0c2a24a674c7b823335
muhammad-shahin <muhammadshahin002@gmail.com>
2023-09-15 19:23:57 +0600

    unnecessary comment removed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 export const Header = () => {
   return (
     <header>
-        <h1 className="text-center text-3xl font-bold py-12">Programming Course Registration</h1>
+      <h1 className="text-center text-3xl font-bold py-12">
+        Programming Course Registration
+      </h1>
     </header>
-  )
-}
+  );
+};
--- src/components/Sidebar/SelectedCourse.jsx
+++ src/components/Sidebar/SelectedCourse.jsx
@@ -1,19 +1,20 @@
 import PropTypes from "prop-types";
 
-const SelectedCourse = ({selectedCourse, count}) => {
+const SelectedCourse = ({ selectedCourse, count }) => {
+  const { course_name } = selectedCourse;
 
-    const {course_name} = selectedCourse;
-    
-    return (
-        <>
-            <h2 className="text-[#1C1B1B99] font-medium text-[18px]">{count}. {course_name}</h2>
-        </>
-    );
+  return (
+    <>
+      <h2 className="text-[#1C1B1B99] font-medium text-[18px]">
+        {count}. {course_name}
+      </h2>
+    </>
+  );
 };
 
 SelectedCourse.propTypes = {
-    selectedCourse: PropTypes.object.isRequired,
-    count: PropTypes.number.isRequired,
-  };
+  selectedCourse: PropTypes.object.isRequired,
+  count: PropTypes.number.isRequired,
+};
 
 export default SelectedCourse;
