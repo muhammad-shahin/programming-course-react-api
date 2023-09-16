d3ef5e3dd5f4dcd906940f2ca7cbfe2b46aa3977
muhammad-shahin <muhammadshahin002@gmail.com>
2023-09-16 14:17:29 +0600

    modal background blur added

--- src/components/utilities/CustomToast/Modal.jsx
+++ src/components/utilities/CustomToast/Modal.jsx
@@ -14,24 +14,33 @@ const Modal = ({ modalStatus, title, message, handleModalCloseBtn }) => {
   }, [modalStatus]);
 
   return (
-    <dialog open ref={modal} id="modal" className="">
-      <img
-        src={infoIcon}
-        alt="Info Icon"
-        className="relative mx-auto -top-4 w-[48px]"
-      />
-      <div className="p-5 text-center space-y-4">
-        <h2 className="text-3xl">{title}</h2>
-        <p className="font-normal text-[18px] max-w-[350px]">
-          {message} <br /> Thank You 🙂{" "}
-        </p>
+    <dialog
+      open
+      id="modal"
+      ref={modal}
+      className="sticky top-0 left-0 w-full h-full bg-transparent"
+    >
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="w-fit bg-white rounded-lg">
+          <img
+            src={infoIcon}
+            alt="Info Icon"
+            className="relative mx-auto -top-4 w-[48px]"
+          />
+          <div className="p-5 text-center space-y-4">
+            <h2 className="text-3xl">{title}</h2>
+            <p className="font-normal text-[18px] max-w-[350px]">
+              {message} <br /> Thank You 🙂{" "}
+            </p>
+          </div>
+          <button
+            onClick={handleModalCloseBtn}
+            className="text-center bg-sky-600 text-white text-xl w-full py-2 hover:bg-rose-500 transition-all rounded-b-lg"
+          >
+            Close
+          </button>
+        </div>
       </div>
-      <button
-        onClick={handleModalCloseBtn}
-        className="text-center bg-sky-600 text-white text-xl w-full py-2 hover:bg-rose-500 transition-all"
-      >
-        Close
-      </button>
     </dialog>
   );
 };
